97bfa8eb13ac53188f8ec6a5a608b0662a907921
Yukiiro-Nite <e.kazuma@gmail.com>
2020-02-20 23:01:59 -0500

    Month of shaders - Day 20: Added ability to rotate reflection

--- src/shaders/day-19/day-19.glsl
+++ src/shaders/day-19/day-19.glsl
@@ -5,6 +5,7 @@ uniform vec2 inputPosition;
 uniform float inputDirection;
 const float PI = 3.14159265359;
 const float TAU = PI * 2.0;
+const float EPSILON = 0.0000001;
 
 float greaterThan(float a, float b) {
   return ceil((sign(a - b) + 1.0) / 2.0);
@@ -14,8 +15,45 @@ float lessThan(float a, float b) {
   return floor((sign(b - a) + 1.0) / 2.0);
 }
 
-vec2 getReflectedPosition(vec2 pos, vec2 reflectionStart, float direction) {
-  return pos * lessThan(pos.x, reflectionStart.x) + (pos - vec2(pos.x - reflectionStart.x, 0.0) * 2.0) * greaterThan(pos.x, reflectionStart.x);
+mat2 rotate2d(float _angle){
+  return mat2(cos(_angle), -sin(_angle),
+              sin(_angle), cos(_angle));
+}
+
+float lineCrossProduct(vec2 pos, vec2 lineA, vec2 lineB) {
+  float dx1 = lineB.x - lineA.x;
+  float dy1 = lineB.y - lineA.y;
+  float dx2 = pos.x - lineB.x;
+  float dy2 = pos.y - lineB.y;
+  
+  return (dx1 * dy2) - (dy1 * dx2);
+}
+
+float signNoZero(float num) {
+  return float(num >= 0.0) * 1.0 + float(num < 0.0) * -1.0;
+}
+
+float avoidZero(float num) {
+  float aboveEpsilon = float(abs(num) >= EPSILON);
+  float belowEpsilon = float(abs(num) < EPSILON);
+  float signedEpsilon = EPSILON * signNoZero(num);
+
+  return (num * aboveEpsilon) + (signedEpsilon * belowEpsilon);
+}
+
+vec2 getReflectedPosition(vec2 pos, vec2 lineStart, float direction) {
+  vec2 lineEnd = lineStart + vec2(cos(direction), sin(direction));
+  float slope = (lineEnd.y - lineStart.y) / avoidZero(lineEnd.x - lineStart.x);
+  float yIntercept = - (slope * lineStart.x - lineStart.y);
+
+  float rX = ((1.0 - pow(slope, 2.0)) * pos.x + 2.0 * slope * pos.y - 2.0 * slope * yIntercept) / (pow(slope, 2.0) + 1.0);
+  float rY = ((pow(slope, 2.0) - 1.0) * pos.y + 2.0 * slope * pos.x + 2.0 * yIntercept) / (pow(slope, 2.0) + 1.0);
+  vec2 reflectedPosition = vec2(rX, rY);
+  float insideOutsideReflection = lineCrossProduct(pos, lineStart, lineEnd);
+  float outsideReflection = float(insideOutsideReflection <= 0.0);
+  float insideReflection = float(insideOutsideReflection > 0.0);
+
+  return (pos * outsideReflection) + (reflectedPosition * insideReflection);
 }
 
 void main() {

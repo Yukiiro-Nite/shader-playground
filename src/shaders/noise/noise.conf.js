cd97aeb1ea2e3c52e1b55d7fc0e4ade883976229
Michael Bryant <e.kazuma@gmail.com>
2020-08-07 17:09:45 -0400

    Added example of 3D simplex noise
    Most of the code was from here: https://github.com/stegu/webgl-noise/blob/master/src/noise3D.glsl

---
+++ src/shaders/noise/noise.conf.js
@@ -0,0 +1,10 @@
+import fragmentShader from './noise.glsl'
+import view from './noise.html';
+
+const config = {
+  name: 'Simplex Noise',
+  frag: fragmentShader,
+  html: view
+}
+
+export default config

b86f3ba4b081e52cbdaf08a5436ceb8428427605
Yukiiro-Nite <e.kazuma@gmail.com>
2020-10-05 21:18:38 -0400

    Added example of cellular noise

---
+++ src/shaders/cellular-noise/cellular-noise.conf.js
@@ -0,0 +1,10 @@
+import fragmentShader from './cellular-noise.glsl'
+import view from './cellular-noise.html';
+
+const config = {
+  name: 'Cellular Noise',
+  frag: fragmentShader,
+  html: view
+}
+
+export default config
